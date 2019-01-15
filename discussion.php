363728175b82977208f3d2d01b3417d914671da8
Sergo23334 <42577184+Sergo23334@users.noreply.github.com>
2019-01-15 15:45:49 +0200

    Add files via upload

--- discussion.php
+++ discussion.php
@@ -25,35 +25,39 @@
 			
 			<div id="centerCol">
 				<div id="discussion">
-					<h1><?= $data["name"] ?></h1>
-					<p><?= $data["discription"] ?></p>
-					<?php if($data["code"] != ""){ ?>
-						<h1>Код:</h1>
-						<pre><?= $data["code"] ?></pre>
-					<?php }; ?>
-					<p align="right"><span><?= $data["date"] ?></span>Автор темы: <?= $data["user"] ?>.</p>
-
-					<?php if(isset($_SESSION["auth"])){ ?>
-						<form id="commentForm" action="" method="POST"><textarea id="comment" placeholder="Ваш коментарий..." type="text"></textarea><textarea id="code" placeholder="Ваш код..." type="text"></textarea><div><input id="goComment" value="Отправить" type="button"><input value="Прикрепить код" id="addCode" type="button"></div></form></br></br>
-					<?php }else{ ?>
-						<p><a style="color: rgb(50, 50, 50);" href="#top">Войдите</a>, чтобы прокоментировать.</p>
-					<?php }; ?>
-					
-					<div id="commentList">
-						<?php if(isset($comments[0]["id"])){ ?>
-							<h1>Коментарии</h1>
-						<?php }; ?>		
-										
-						<?php for($i = 0; $i < count($comments); $i++){ ?>
-							<div id="commentBox">
-								<b <?php if($comments[$i]["user"] == $data["user"]) echo "style='color: rgb(0, 90, 0);'" ?>><?= $comments[$i]["user"] ?></b></br>
-								<span><?= $comments[$i]["comment"] ?></span>
-								<?php if($comments[$i]["code"] != ""){ ?>
-									<pre><?= $comments[$i]["code"] ?></pre>
-								<?php }; ?>
-							</div>	
+					<?php if(isset($data)){ ?>
+						<h1><?= $data["name"] ?></h1>
+						<p><?= $data["discription"] ?></p>
+						<?php if($data["code"] != ""){ ?>
+							<h1>Код:</h1>
+							<pre><?= $data["code"] ?></pre>
 						<?php }; ?>
-					</div>
+						<p align="right"><span><?= $data["date"] ?></span>Автор темы: <?= $data["user"] ?>.</p>
+
+						<?php if(isset($_SESSION["auth"])){ ?>
+							<form id="commentForm" action="" method="POST"><textarea id="comment" placeholder="Ваш коментарий..." type="text"></textarea><textarea id="code" placeholder="Ваш код..." type="text"></textarea><div><input id="goComment" value="Отправить" type="button"><input value="Прикрепить код" id="addCode" type="button"></div></form></br></br>
+						<?php }else{ ?>
+							<p><a style="color: rgb(50, 50, 50);" href="#top">Войдите</a>, чтобы прокоментировать.</p>
+						<?php }; ?>
+						
+						<div id="commentList">
+							<?php if(isset($comments[0]["id"])){ ?>
+								<h1>Коментарии</h1>
+							<?php }; ?>		
+											
+							<?php for($i = 0; $i < count($comments); $i++){ ?>
+								<div id="commentBox">
+									<b <?php if($comments[$i]["user"] == $data["user"]) echo "style='color: rgb(0, 90, 0);'" ?>><?= $comments[$i]["user"] ?></b></br>
+									<span><?= $comments[$i]["comment"] ?></span>
+									<?php if($comments[$i]["code"] != ""){ ?>
+										<pre><?= $comments[$i]["code"] ?></pre>
+									<?php }; ?>
+								</div>	
+							<?php }; ?>
+						</div>
+					<?php }else{ ?>
+						<h1 align="center">Записи не существует!</h1>
+					<?php }; ?>
 				</div>
 			</div>
 
